d28c413fa6361111e637f7f84472e2044b1e51e5
Seven Y Q Guo <superShy77@163.com>
2022-06-27 15:59:33 +0800

    make it work

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 sink('some module', function (test, ok) {
-  // write tests
+  test('a failure', function () {
+    ok(1 == 2, 'should fail')
+  })
 })
 
 sink('another module', function (test, ok) {
-  // write tests
+  test('should have foo', function () {
+    ok(true, 'this is basically true')
+    ok(1 == 1, 'also true for you math majors')
+  })
 })
 
-function sink() {}
+function sink(name, cb) {
+  cb(test, ok);
+}
+function test(description, cb) {
+  cb()
+}
+function ok(isTrue, message) {
+  if (isTrue) {
+    console.log(message);
+  } else {
+    console.error(message);
+  }
+}
